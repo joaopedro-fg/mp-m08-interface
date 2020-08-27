03fca5f24102f8565b9791760b0c82ea9a7bc45c
João Pedro <joao@LAPTOP-TF28SHQR.localdomain>
2020-08-27 10:41:22 -0300

    form dentro do modal funcionando

--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from . import db
 from flask_login import login_required, current_user
 
@@ -8,7 +8,9 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
     
-@main.route('/profile')
+@main.route('/profile', methods=['POST', 'GET'])
 @login_required
 def profile():
+    if request.method == 'POST':
+        print(request.form['info3'])
     return render_template('profile.html', name=current_user.name) 
