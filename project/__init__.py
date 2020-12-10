0aa9e752e542e6b278d5f9c5b92deddaab759986
João Pedro <joao@LAPTOP-TF28SHQR.localdomain>
2020-12-10 02:48:37 -0200

    Sistema de Arquivos

--- project/__init__.py
+++ project/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/assets', static_folder='assets')
 
     app.config['SECRET_KEY'] = 'secret_key_09102'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -26,3 +26,6 @@ def create_app():
     app.register_blueprint(main_blueprint)
 
     return app
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
